2fd82fc58e923c40a27fed219f9e4b2edc2b0c45
millieb <mildred.brito@outlook.com>
2019-11-03 08:55:21 -0700

    First Commit

---
+++ lab1_makeup/example/lab1_makeup_example.dart
@@ -0,0 +1,6 @@
+import 'package:lab1_makeup/lab1_makeup.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lab1_makeup/lib/lab1_makeup.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lab1_makeup;
+
+export 'src/lab1_makeup_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
